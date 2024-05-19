b885c09f494b00f754217a2338d9daa267d23dbd
dewi-xaltius <smdewi@xaltius.tech>
2024-05-19 23:39:58 +0800

    add exercise for changing className

---
+++ module4/mod4-exercise2.js
@@ -0,0 +1,9 @@
+function setEasyMode() {
+    let gameMode = document.getElementById("gameMode");
+    gameMode.className = "easy-mode";
+  }
+
+function setHardMode() {
+    let gameMode = document.getElementById("gameMode");
+    gameMode.className = "hard-mode";
+  }
