2d40dfca09a19cfd634d17d5cc92e793117346b2
dewi-xaltius <smdewi@xaltius.tech>
2024-05-19 16:00:38 +0800

    add exercise 1-3

---
+++ module4/mod4-exercise1.js
@@ -0,0 +1,30 @@
+// Module 4 - Learning Exercise 4.1
+
+// Exercise 1 - Declare three variables: one to store your name, 
+// one to store your age, and one to store whether you are a student.
+console.log("Exercise 1")
+let studentName = "John Doe";
+let age = 11;
+let isStudent = true;
+
+console.log("Student Name: ", studentName);
+console.log("Student Age: ", age);
+console.log("Is this student enrolled: ", isStudent);
+
+// Exercise 2 - Create two variables to store numbers and 
+// then create a third variable that stores the sum of 
+// the first two variables. Print the result to the console.
+console.log("Exercise 2")
+let num1 = 5;
+let num2 = 10;
+let result = num1 + num2;
+console.log("The result is: ", result)
+
+// Exercise 3 - Declare two string variables and concatenate 
+// them into a third variable. Print the concatenated string 
+// to the console. 
+console.log("Exercise 3")
+let firstName = "Jane";
+let lastName = "Doe"
+let fullName = firstName + " " + lastName
+console.log(fullName);
