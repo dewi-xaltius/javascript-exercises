ab113b03f50620799b6dd03c51b2868610720586
dewi-xaltius <smdewi@xaltius.tech>
2024-05-20 09:44:32 +0800

    add module 7

--- module5/mod5-exercise1.js
+++ module5/mod5-exercise1.js
@@ -61,11 +61,4 @@ let threshold = 100;
 let filteredExpenses = expenses.filter(expense => expense > threshold);
 console.log("Filtered expenses:", filteredExpenses); // [150, 250, 300]
 
-// Task 5: Create a function to increase each expense by a given percentage and return the new array.
-function increaseExpenses(expenses, percentage) {
-  return expenses.map(expense => expense * (1 + percentage / 100));
-}
-
-let increasedExpenses = increaseExpenses(expenses, 10);
-console.log("Increased expenses by 10%:", increasedExpenses); // [55, 110, 165, 275, 330]
 
