a3b44c7c2d18a5743209a1fbd606c392a4e91095
dewi-xaltius <smdewi@xaltius.tech>
2024-05-20 10:24:51 +0800

    add module 6 exercise 3 else if statement

--- module6/mod6-exercise1.js
+++ module6/mod6-exercise1.js
@@ -1,5 +1,6 @@
-// Exercise 1
+// Exercise 1: Check for minimum age requirements
 // Check if the user meets the age requirement to register on the website
+console.log("Exercise 1")
 let userAge = 17;
 let minAge = 18;
 if (userAge >= minAge) {
@@ -7,3 +8,32 @@ if (userAge >= minAge) {
 } else {
   console.log("User is too young to register.");
 }
+
+// Exercise 2: Check user login
+console.log("Exercise 2")
+const correctPassword = "javascript123";
+const userInput = "javascript123"; 
+
+if (userInput === correctPassword) {
+  console.log("Access granted");
+} else {
+  console.log("Access denied");
+}
+
+// Exercise 3: else if Statement
+console.log("Exercise 3")
+const score = 85; // Student's score
+
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else if (score >= 70) {
+  console.log("C");
+} else if (score >= 60) {
+  console.log("D");
+} else {
+  console.log("F");
+}
+
+  
